6371f8938211b6d9398be2ee53356e15d9c7b2af
petr <py0trandreev@yandex.ru>
2021-10-31 15:06:27 +0300

    Lesson 2. Templates

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 import mainapp.views as mainapp
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", mainapp.main),
-    path("products/", mainapp.products),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("products/", mainapp.products, name="products"),
+    path("contact/", mainapp.contact, name="contact"),
 ]
