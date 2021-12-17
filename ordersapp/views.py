c7ec009dacb96494e074d6ef787b7a282faafde7
py0trandreev <79420905+py0trandreev@users.noreply.github.com>
2021-12-17 21:09:02 +0300

    Lesson14 (#16)
    * lesson_14. debug-tool-bar
    
    * lesson_14. Testing tools
    
    * lesson_14. Load testing
    
    * lesson_14. Query renew
    
    * lesson_14. DB twiks
    
    * lesson_14. Code style fix
    
    * screenshots copied

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
@@ -12,7 +13,7 @@ from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
 
 
-class OrderList(ListView):
+class OrderList(LoginRequiredMixin, ListView):
     model = Order
 
     def get_queryset(self):
@@ -82,10 +83,12 @@ class OrderItemsUpdate(UpdateView):
     def get_context_data(self, **kwargs):
         data = super(OrderItemsUpdate, self).get_context_data(**kwargs)
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
         if self.request.POST:
             data["orderitems"] = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            formset = OrderFormSet(instance=self.object)
+            queryset = self.object.orderitems.select_related()
+            formset = OrderFormSet(instance=self.object, queryset=queryset)
             for form in formset.forms:
                 if form.instance.pk:
                     form.initial["price"] = form.instance.product.price
@@ -121,7 +124,7 @@ def order_forming_complete(request, pk):
 
     return HttpResponseRedirect(reverse("ordersapp:orders_list"))
 
-  
+
 @receiver(pre_save, sender=OrderItem)
 @receiver(pre_save, sender=Basket)
 def product_quantity_update_save(instance, sender, **kwargs):
@@ -140,15 +143,3 @@ def product_quantity_update_delete(instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
 
-
-
-from mainapp.models import Product
-from django.http import JsonResponse
-
-def get_product_price(request, pk):
-    if request.is_ajax():
-        product = Product.objects.filter(pk=int(pk)).first()
-        if product:
-            return JsonResponse({"price": product.price})
-        else:
-            return JsonResponse({"price": 0})
